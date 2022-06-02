831bf81d771e5725d3ad8e26d6a3b97b9b9bd279
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-03 02:18:11 +0900

    spring initializr

---
+++ hajiboot/src/main/java/com/example/hajiboot/HajibootApplication.java
@@ -0,0 +1,13 @@
+package com.example.hajiboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HajibootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HajibootApplication.class, args);
+	}
+
+}
