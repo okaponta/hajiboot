711f969c267335d6c1b2b9ad25e245a0efbd17be
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-03 04:05:27 +0900

    add sample program

--- hajiboot-layering/src/main/java/com/example/HajibootLayeringApplication.java
+++ hajiboot-layering/src/main/java/com/example/HajibootLayeringApplication.java
@@ -1,10 +1,26 @@
 package com.example;
 
+import com.example.domain.Customer;
+import com.example.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HajibootLayeringApplication {
+public class HajibootLayeringApplication implements CommandLineRunner {
+
+	@Autowired
+	CustomerService customerService;
+
+	@Override
+	public void run(String... args) throws Exception {
+		customerService.save(new Customer(1,"Nobita","Nobi"));
+		customerService.save(new Customer(2,"Takeshi","Goda"));
+		customerService.save(new Customer(3,"Suneo","Honekawa"));
+
+		customerService.findAll().forEach(System.out::println);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HajibootLayeringApplication.class, args);
