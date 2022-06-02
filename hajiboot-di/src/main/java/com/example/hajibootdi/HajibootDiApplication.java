23bb568acb71091943b579f5e30285fc4b81ef57
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-03 02:56:29 +0900

    spring initializr hajiboot-di

---
+++ hajiboot-di/src/main/java/com/example/hajibootdi/HajibootDiApplication.java
@@ -0,0 +1,13 @@
+package com.example.hajibootdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HajibootDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HajibootDiApplication.class, args);
+	}
+
+}
---
+++ hajiboot-di/src/test/java/com/example/hajibootdi/HajibootDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hajibootdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HajibootDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
