b7990e6177efd5886614848cba478a2579103ea2
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-03 03:44:15 +0900

    implement command line runner

--- hajiboot-di/src/main/java/com/example/HajibootDiApplication.java
+++ hajiboot-di/src/main/java/com/example/HajibootDiApplication.java
@@ -1,17 +1,32 @@
 package com.example;
 
-import com.example.app.Frontend;
+import com.example.app.Argument;
+import com.example.app.ArgumentResolver;
+import com.example.app.Calculator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class HajibootDiApplication {
+public class HajibootDiApplication implements CommandLineRunner {
+
+    @Autowired
+    ArgumentResolver argumentResolver;
+    @Autowired
+    Calculator calculator;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.print("Enter 2 numbers like 'a b' : ");
+        Argument argument = argumentResolver.resolve(System.in);
+
+        int result = calculator.calc(argument.a(), argument.b());
+        System.out.println("result = " + result);
+    }
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(HajibootDiApplication.class, args);
-        Frontend frontend = context.getBean(Frontend.class);
-        frontend.run();
+        SpringApplication.run(HajibootDiApplication.class, args);
     }
 
 }
