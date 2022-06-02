2c5e16d2fd8084700899c8f5921396693f328917
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-03 02:53:06 +0900

    add first method

--- hajiboot/src/main/java/com/example/hajiboot/HajibootApplication.java
+++ hajiboot/src/main/java/com/example/hajiboot/HajibootApplication.java
@@ -2,9 +2,17 @@ package com.example.hajiboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HajibootApplication {
+
+	@GetMapping("/")
+	String home(){
+		return "Hello Spring!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HajibootApplication.class, args);
