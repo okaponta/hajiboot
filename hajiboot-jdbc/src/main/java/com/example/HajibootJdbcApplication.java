7827c386c67354cf3b69a605a9d98c41a527aa6d
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-03 12:04:15 +0900

    spring initializr

---
+++ hajiboot-jdbc/src/main/java/com/example/HajibootJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HajibootJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HajibootJdbcApplication.class, args);
+	}
+
+}
